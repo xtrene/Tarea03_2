e1eb60f87f1205378eb3142d2403c1be4f67069f
Carlos <carlos@xtrene.com>
2021-06-20 22:05:00 +0200

    Agregar archivos de proyecto.

---
+++ Tarea03_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tarea03_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 15;
+            double A = 3.67;
+            char B = 'G';
+
+            Console.WriteLine("Variable N = {0} ", N);
+            Console.WriteLine("Variable A = {0}", A);
+            Console.WriteLine("Variable B = {0}", B);
+
+            Console.WriteLine("{0} + {1} = {2}", N, A, N + A);
+            Console.WriteLine("{0} - {1} = {2}", A, N, A - N);
+            Console.WriteLine("Valor numérico del carácter {0} = {1}", B,(int)B);
+
+            Console.ReadKey();
+        }
+    }
+}
